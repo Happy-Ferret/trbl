41dd26665debf8e4b0650be7f164e3baad92963d
Myk Melez <myk@mykzilla.org>
2018-04-03 15:33:30 -0700

    pass through args to run app specified on command line

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("./firefox")
+    let mut args = std::env::args_os().skip(1).collect::<Vec<std::ffi::OsString>>();
+    args.push(std::ffi::OsString::from("--app"));
+    args.push(std::ffi::OsString::from("../Resources/qbrt/application.ini"));
+
+    Command::new("./firefox")
         .current_dir(std::env::current_exe().unwrap().parent().unwrap())
-        .args(&["--app", "../Resources/qbrt/application.ini"])
-        .output().unwrap_or_else(|e| {
-            panic!("failed to execute process: {}", e)
-    });
-
-    if output.status.success() {
-        let s = String::from_utf8_lossy(&output.stdout);
-
-        print!("command succeeded and stdout was:\n{}", s);
-    } else {
-        let s = String::from_utf8_lossy(&output.stderr);
-
-        print!("command failed and stderr was:\n{}", s);
-    }
+        .args(args)
+        .status().expect("runtime failed");
 }
