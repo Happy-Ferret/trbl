d3f37105443035f513175f0d190de496d6aa473e
Myk Melez <myk@mykzilla.org>
2018-04-03 16:14:48 -0700

    return exit code from spawned process

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use std::ffi::OsString;
-use std::process::Command;
+use std::process::{ Command };
 
 fn main() {
+    let exit_code = spawn_runtime();
+    std::process::exit(exit_code)
+}
+
+fn spawn_runtime() -> i32 {
     let mut args = std::env::args_os().skip(1).collect::<Vec<OsString>>();
     args.push(OsString::from("--app"));
     args.push(OsString::from("../Resources/qbrt/application.ini"));
 
-    Command::new("./firefox")
+    let status = Command::new("./firefox")
         .current_dir(std::env::current_exe().unwrap().parent().unwrap())
         .args(args)
-        .status().expect("runtime failed");
+        .status()
+        .expect("error spawning runtime");
+
+    match status.code() {
+        Some(code) => code,
+        None       => -1,
+    }
 }
