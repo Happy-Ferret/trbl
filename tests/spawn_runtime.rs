fa9d4f13b4bbdf7dd96db8e5ff3408fc0f0fbb61
Myk Melez <myk@mykzilla.org>
2018-04-04 13:43:17 -0700

    check echoed args in addition to exit codes

--- tests/spawn_runtime.rs
+++ tests/spawn_runtime.rs
@@ -1,30 +1,36 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 #[test]
 fn run_trbl() {
-    let status = Command::new("../trbl")
+    let output = Command::new("../trbl")
         .current_dir(std::env::current_exe().unwrap().parent().unwrap())
-        .status()
+        .output()
         .expect("error running trbl");
-    assert_eq!(status.code(), Some(0));
+    assert_eq!(String::from_utf8_lossy(&output.stdout).trim(),
+        r#"["./firefox", "--app", "../Resources/qbrt/application.ini"]"#);
+    assert_eq!(output.status.code(), Some(0));
 }
 
 #[test]
 fn run_trbl_with_arg() {
-    let status = Command::new("../trbl")
+    let output = Command::new("../trbl")
         .current_dir(std::env::current_exe().unwrap().parent().unwrap())
         .arg("foo")
-        .status()
+        .output()
         .expect("error running trbl");
-    assert_eq!(status.code(), Some(0));
+    assert_eq!(String::from_utf8_lossy(&output.stdout).trim(),
+        r#"["./firefox", "foo", "--app", "../Resources/qbrt/application.ini"]"#);
+    assert_eq!(output.status.code(), Some(0));
 }
 
 #[test]
-fn run_trbl_with_exit_code_1() {
-    let status = Command::new("../trbl")
+fn run_trbl_with_exit_code() {
+    let output = Command::new("../trbl")
         .current_dir(std::env::current_exe().unwrap().parent().unwrap())
         .args(&["--exit-code", "1"])
-        .status()
+        .output()
         .expect("error running trbl");
-    assert_eq!(status.code(), Some(1));
+    assert_eq!(String::from_utf8_lossy(&output.stdout).trim(),
+        r#"["./firefox", "--exit-code", "1", "--app", "../Resources/qbrt/application.ini"]"#);
+    assert_eq!(output.status.code(), Some(1));
 }
